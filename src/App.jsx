5384be96354bcc858f4399e4d27b3919c09676ed
kevinjfaucher <kevinjfaucher@gmail.com>
2025-04-07 15:54:51 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(5);
+
+  function incrementUnsafely() {
+    setCount(count + 1) // 5 + 1
+    setCount(count + 1) // 5 + 1
+    setCount(count + 1) // 5 + 1
+    setCount(count + 1) // 5 + 1
+    setCount(count + 1) // 5 + 1
+    setCount(count + 1) // 5 + 1
+  }
+
+  function incrementSafely() {
+    setCount(prev => prev + 1); // 5 + 1
+    setCount(prev => prev + 1); // (6) + 1
+    setCount(prev => prev + 1);// (7) + 1
+    setCount(prev => prev + 1);
+    setCount(prev => prev + 1);
+    setCount(prev => prev + 1);
+    setCount(prev => prev + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={incrementSafely}>Increment (safe)</button>
+      <button onClick={incrementUnsafely}>Increment (risky)</button>
+    </div>
+  );
+
+}
+
+export default App;
